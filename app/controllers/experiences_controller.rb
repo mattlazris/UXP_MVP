96a6f93bffb50e888bf49547cedf0860b96e7096
Jacky (Yuxuan) Jin <jackykimjk@icloud.com>
2018-08-06 16:25:04 +0800

    experience index page

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,25 @@
+class ExperiencesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+    @experiences = Experience.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
