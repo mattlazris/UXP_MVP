321b3875dfbfd85de2c87ecf354fbf66fd0af3a4
Jacky (Yuxuan) Jin <jackykimjk@icloud.com>
2018-08-06 17:17:32 +0800

    user can now see all experiences he is holding as well edit, delete and add experiences

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -10,18 +10,37 @@ class ExperiencesController < ApplicationController
   end
 
   def new
+    if current_user.host?
+      @experience = Experience.new
+    else
+      redirect_to profile_path
+    end
   end
 
   def create
+    @experience = Experience.new(experience_params)
+    @experience.user = current_user
+    if @experience.save
+      redirect_to profile_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @experience.update(experience_params)
+      redirect_to @experience
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @experience.destroy
+    redirect_to profile_path
   end
 
   private
