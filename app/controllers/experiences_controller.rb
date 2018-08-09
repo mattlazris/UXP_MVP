6fbda3f56bb3b67bf30982a83bf4ddb2e4cc4033
Jacky (Yuxuan) Jin <jackykimjk@icloud.com>
2018-08-09 17:54:21 +0800

    a bug fixed

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -56,6 +56,6 @@ class ExperiencesController < ApplicationController
   end
 
   def experience_params
-    params.require(:experience).permit(:name, :description, :price, :capacity, :photo, :location, :language, :category, :duration, :tag_list: [])
+    params.require(:experience).permit(:name, :description, :price, :capacity, :photo, :location, :language, :category, :duration, :tag_list => [])
   end
 end
